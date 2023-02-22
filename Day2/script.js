e70eb69961f48fb8d6d8652cd7e75fb11ac2ab3c
SphinxBSD <fav6519@gmail.com>
2023-02-22 01:29:13 -0400

    guardando respuestas nivel 2, dia 2

--- Day2/script.js
+++ Day2/script.js
@@ -29,3 +29,49 @@ console.log(challenge.startsWith("30"));
 console.log(challenge.endsWith("Script"));
 console.log(concatenacion.concat("30 Days of ", "JavaScript"));
 console.log(challenge.repeat(2));
+
+//Level 2
+let quote1 = "There is no exercise better for the heart than reaching down and lifting people up.";
+let quote2 =
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+
+let num10s = "10";
+let num10i = 10;
+
+console.log(typeof num10s);
+console.log(typeof num10i);
+
+let num10sInt = parseInt(num10s);
+console.log(typeof num10sInt);
+
+let word1 = "python";
+let word2 = "jargon";
+let wordsearched = "on";
+let level2sentence = "I hope this course is not full of jargon.";
+
+console.log(word1.includes(wordsearched) && word2.includes(wordsearched));
+console.log(level2sentence.includes("jargon"));
+
+console.log(parseInt(Math.random() * 100));
+console.log(parseInt(Math.random() * 50) + 50);
+console.log(parseInt(Math.random() * 255));
+
+let palabra = "JavaScript";
+console.log(palabra.charAt(parseInt(Math.random() * (palabra.length - 1))));
+
+let numeros = [1, 2, 3, 4, 5];
+let lim = 3;
+let cadena = "";
+for (let i = 0; i < numeros.length; i++) {
+  let temp = 1;
+  cadena = cadena + numeros[i] + "\t" + temp;
+  for (let j = 0; j < lim; j++) {
+    temp = temp * numeros[i];
+    cadena = cadena + "\t" + temp;
+  }
+  cadena = cadena + "\n";
+}
+console.log(cadena);
+let primero = sentence.indexOf("because");
+let ultimo = sentence.lastIndexOf("because");
+console.log(sentence.substring(primero, ultimo + 7));
